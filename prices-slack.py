62a91ebf923d830d42f8f93da2b7cd8327764097
eslem <j.eslem03@gmail.com>
2017-12-22 18:41:22 +0100

    added bot python

---
+++ prices-slack.py
@@ -0,0 +1,44 @@
+import json
+
+import pandas as pd
+pd.set_option('precision', 2)
+
+import requests
+import time
+
+intereses = ['bitcoin', 'iota', 'ethereum', 'litecoin']
+column_names = ['name','price_eur', 'percent_change_24h', 'percent_change_7d']
+
+def formatData(df, column_names = ['name','price_eur', 'percent_change_24h', 'percent_change_7d']):
+    text = ''
+    for index, row in df.iterrows():
+        for i in column_names:
+            print(i)
+            text = text+str(row[i])+' '
+        text = text + '\n'
+    print(text)
+
+def sendSlackData(df):
+    webhook_url = 'https://hooks.slack.com/services/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'
+    slack_data = {'text': df.to_string(index  = False, col_space  = 15)+'\n\n'}
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        print("Error sending data")
+    else:
+        print("Data sent")
+        
+api = 'https://api.coinmarketcap.com/v1/ticker?convert=EUR&limit=10?'        
+def sendPrices():
+    import requests
+    r = requests.get(api)
+    if r.status_code != 200:
+        print("No available data")
+    df = pd.read_json(r.text)
+    df = df[df.id.isin(intereses)]
+    df = df[column_names]
+    sendSlackData(df)
+    
+sendPrices()
